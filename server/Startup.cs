2ac6a5febb351e6954a608e894e7ab7222d8e7f0
Jonathan Rupp <jrupp@rightpoint.com>
2020-02-25 16:56:18 -0600

    Progress on passthrough auth

--- server/Startup.cs
+++ server/Startup.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace server
 {
@@ -27,16 +29,18 @@ namespace server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(c => c.AddConsole());
+
             services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
                 .AddAzureAD(options => Configuration.Bind("AzureAd", options));
 
-            services.AddRazorPages().AddMvcOptions(options =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-                options.Filters.Add(new AuthorizeFilter(policy));
-            });
+            //services.AddRazorPages().AddMvcOptions(options =>
+            //{
+            //    var policy = new AuthorizationPolicyBuilder()
+            //        .RequireAuthenticatedUser()
+            //        .Build();
+            //    options.Filters.Add(new AuthorizeFilter(policy));
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,18 +58,37 @@ namespace server
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            //app.UseStaticFiles();
 
-            app.UseRouting();
+            //app.UseRouting();
+
+            app.Use(async (context, next) =>
+            {
+                var log = context.RequestServices.GetService<ILogger<Program>>();
+                log.LogInformation(context.Request.GetDisplayUrl());
+                if (context.Request.GetDisplayUrl().EndsWith("/signin-oidc"))
+                {
+                    await next();
+                    return;
+                }
+                var auth = await context.AuthenticateAsync();
+                if (!auth.Succeeded)
+                {
+                    await context.ChallengeAsync();
+                    return;
+                }
+                log.LogInformation(auth.Succeeded.ToString());
+                await next();
+            });
 
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapRazorPages();
+            //    endpoints.MapControllers();
+            //});
         }
     }
 }
